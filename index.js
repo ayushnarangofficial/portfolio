99e95f2a49ddbb380adba8a18c438a5d4802b0d2
Ayush Narang <108880041+ayushnarangofficial@users.noreply.github.com>
2022-09-23 02:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+
+var autoType = new Typed(".auto", {
+  strings: ["Ayush Narang,"],
+  typeSpeed: 100,
+  backSpeed: 120,
+  loop: true,
+});
+
+
+const scrollAnimation = ScrollReveal ({
+  distance: '60px',
+  duration: 2500,
+  reset: true
+});
+
+scrollAnimation.reveal('.main-one', {delay:100, origin: 'left'});
+scrollAnimation.reveal('.main-two', {delay:170, origin: 'right'});
+scrollAnimation.reveal('.projects-info', {delay:100, origin: 'left'});
+scrollAnimation.reveal('.row-one', {delay:170, origin: 'left'});
+scrollAnimation.reveal('.row-two', {delay:170, origin: 'right'});
+scrollAnimation.reveal('.about-one', {delay:100, origin: 'left'});
+scrollAnimation.reveal('.about-two', {delay:100, origin: 'right'});
+scrollAnimation.reveal('.contact', {delay:100, origin: 'top'});
+
+
+
 const body = document.querySelector("body"),
 nav = document.querySelector("nav"),
 modeToggle = document.querySelector(".dark-light"),
@@ -33,10 +59,3 @@ nav.classList.remove("active");
 }
 });
 
-
-var autoType = new Typed(".auto", {
-  strings: ["Ayush Narang,"],
-  typeSpeed: 100,
-  backSpeed: 120,
-  loop: true,
-});
